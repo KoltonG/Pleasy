c09102f6c3f1cf48dc6883e280208765f4d511d1
Kolton Gagnon <Kolton.Gagnon@Gmail.com>
2014-11-30 13:09:50 -0400

    Implemented Course Name and Hover for selected

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,7 +2,7 @@ class DashboardController < ApplicationController
 
   def BCS
     @core_courses = Course.joins('LEFT JOIN course_details ON courses.number = course_details.number AND courses.dept = course_details.dept ').
-        select("courses.dept, courses.number, course_details.description").
+        select("courses.dept, courses.number, course_details.description, course_details.name").
         where("(courses.dept = 'CS' or courses.dept = 'INFO') and courses.city = 'Fredericton' and (courses.number ='1073' or courses.number = '1083' or courses.number = '1103' or courses.number = '1303'
               or courses.number = '2043' or courses.number = '2253' or courses.number = '2333'
               or courses.number = '2383' or courses.number = '3383' or courses.number = '3413'
@@ -10,14 +10,14 @@ class DashboardController < ApplicationController
         uniq.order("dept, number")
 
     @tech_courses = Course.joins('LEFT JOIN course_details ON courses.number = course_details.number AND courses.dept = course_details.dept ').
-        select("courses.dept, courses.number, course_details.description").
+        select("courses.dept, courses.number, course_details.description, course_details.name").
         where("(courses.dept = 'CS' or courses.dept = 'INFO' or courses.dept = 'SWE') and courses.city = 'Fredericton' and  (
-        CONCAT ( courses.dept, courses.number ) not in ('CS1073','CS1083','CS1303','CS2043','CS2253','CS2333','CS2383','CS3383','CS3413','CS3853','CS3873','CS3997', 'INFO1103')
+        CONCAT ( courses.dept, courses.number ) not in ('CS1073','CS1083','CS1303','CS2043','CS2253','CS2333','CS2383','CS3383','CS3413','CS3853','CS3873','CS3997', 'INFO1103', 'CS5865', 'CS6605', 'CS6705', 'CS6735', 'CS6795', 'CS6895', 'CS6905', 'CS6991', 'CS6996', 'CS6997', 'CS6025', 'CS6035', 'CS6075', 'CS6355')
         ) and courses.credits >= 3").
         uniq.order("dept, number")
 
     @math_stat_courses = Course.joins('LEFT JOIN course_details ON courses.number = course_details.number AND courses.dept = course_details.dept ').
-        select("courses.dept, courses.number, course_details.description").
+        select("courses.dept, courses.number, course_details.description, course_details.name").
         where("(courses.dept = 'MATH' or courses.dept = 'STAT') and courses.city = 'Fredericton' and (courses.number ='1003' or courses.number = '1013' or courses.number = '1503' or courses.number = '2213'
               or courses.number = '2593' or courses.number = '3083' or courses.number = '3033'
               or courses.number = '3063' or courses.number = '3093' or courses.number = '3213'
