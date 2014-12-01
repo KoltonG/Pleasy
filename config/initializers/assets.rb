b3bbbc2851d9d6ce1d36009d4a4ca75b75af1510
Nakah Nakah <test@test.com>
2014-12-01 13:08:40 -0400

    Signup page styled, error messages styled and replace under-logo-text

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,8 +1,10 @@
-Rails.application.config.assets.precompile += %w( pleasy.css )
+
 Rails.application.config.assets.precompile += %w( init.js )
 Rails.application.config.assets.precompile += %w( skel.min.js )
 Rails.application.config.assets.precompile += %w( login.css )
+Rails.application.config.assets.precompile += %w( register.css )
 
+Rails.application.config.assets.precompile += %w( pleasy.css )
 Rails.application.config.assets.precompile += %w( demo.css )
 Rails.application.config.assets.precompile += %w( font-awesome.css )
 Rails.application.config.assets.precompile += %w( menu_topside.css )
